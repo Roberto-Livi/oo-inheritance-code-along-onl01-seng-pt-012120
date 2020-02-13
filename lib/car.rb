85c851d6655ee94802646cd1a9613194592b75a1
Roberto Livi <robertomlivi@outlook.com>
2020-02-13 23:24:11 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,28 @@
 require_relative "./vehicle.rb"
+
+class Car
+  
+  attr_accessor :wheel_size, :number
+  
+  def initialize(wheel_size, number)
+    @wheel_size = wheel_size
+    @number = number
+  end
+  
+  def wheel_size
+    self.wheel_size
+  end
+  
+  def wheel_number
+    self.number
+  end
+  
+  def go
+    'VRRROOOOOOOOOOOOOOOOOOM!!!!!'
+  end
+  
+  def fill_up_tank
+    'filling up!'
+  end
+  
+end
